89dc456ac5627bafe20cd8bf9aacdb7780d49e34
Sonukumar2003 <149610767+Sonukumar2003@users.noreply.github.com>
2026-01-11 16:52:22 +0530

    cors fix

--- frontend/src/components/Reservation.jsx
+++ frontend/src/components/Reservation.jsx
@@ -18,7 +18,7 @@ const Reservation = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:4000/api/v1/reservations/send",
+        "https://restraw.onrender.com/api/v1/reservations/send",
         {
           firstName,
           lastName,
